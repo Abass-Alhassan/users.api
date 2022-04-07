eaf6904b3d70505a76e2e84004781ef1597b370c
Abass-Alhassan <icewrldspacely@gmail.com>
2022-04-07 08:18:01 -0700

    first commit

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1, name:"John", age:25},
+    {id:2, name:"Andy", age:47},
+    {id:3, name:"Joseph", age:100},
+    {id:4, name:"Jonathan", age:46},
+    {id:5, name:"Jones", age:20}
+]
+module.exports=users
